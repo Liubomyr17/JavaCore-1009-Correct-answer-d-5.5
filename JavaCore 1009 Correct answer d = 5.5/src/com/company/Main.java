51605c2c3d79558162fe3a16f160d013041db609
Code First! <41821541+Liubomyr17@users.noreply.github.com>
2020-02-25 10:42:39 +0200

    Add files via upload

---
+++ JavaCore 1009 Correct answer d = 5.5/src/com/company/Main.java
@@ -0,0 +1,54 @@
+package com.company;
+
+/*
+
+1009 Correct answer: d = 5.5
+Add one type conversion operation to get the answer: d = 5.5.
+
+Requirements:
+1. The program should display the text on the screen.
+2. You cannot change the display command.
+3. The main () method must contain a variable of type int.
+4. The main () method must contain a variable b of type int.
+5. The main () method must contain a variable c of type int.
+6. The main () method must contain an e variable of type int.
+7. The main () method must contain a d variable of type double.
+8. The initial value of the variables during initialization cannot be changed. You can add only cast operators.
+9. The program should output the number 5.5.
+
+ */
+
+import java.io.*;
+import java.lang.reflect.Array;
+import java.net.URISyntaxException;
+import java.nio.file.FileSystemException;
+import java.nio.file.attribute.AclFileAttributeView;
+import java.rmi.RemoteException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 50;
+        int b = 17;
+        double d = (double) a / b;
+        System.out.println(d);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
